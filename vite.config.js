43fdc339d9c133f63f6889f86fbd1c6249562de9
Lin Lin Aung <linlinaung@Lins-Mac-mini.local>
2025-04-22 23:20:48 +0630

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],base: process.env.VITE_BASE_PATH || '/'
+  plugins: [react(),tailwindcss()],base: process.env.VITE_BASE_PATH || '//portfolio_web'
 })
